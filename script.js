20d076ff55c12b3290008774026bde77b0cfc072
yanyu612 <115054758+yanyu612@users.noreply.github.com>
2025-02-17 15:52:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -49,10 +49,10 @@ noButton.addEventListener("click", function () {
   }
 
   // 图片变化（前 5 次变化）
-  if (clickCount === 1) mainImage.src = "images/sad1.png"; // 震惊
-  if (clickCount === 2) mainImage.src = "images/sad2.png"; // 思考
-  if (clickCount === 3) mainImage.src = "images/sad3.png"; // 生气
-  if (clickCount === 4) mainImage.src = "images/sad4.png"; // 哭
+  if (clickCount == 1) mainImage.src = "images/sad1.png"; // 震惊
+  if (clickCount == 2) mainImage.src = "images/sad2.png"; // 思考
+  if (clickCount == 3) mainImage.src = "images/sad3.png"; // 生气
+  if (clickCount == 4) mainImage.src = "images/sad4.png"; // 哭
   if (clickCount >= 5) mainImage.src = "images/sad5.png"; // 之后一直是哭
 });
 
